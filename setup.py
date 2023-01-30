a9e41c4511b71a4292b27ab78b376cd4be91df8b
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2023-01-30 11:22:20 -0600

    PXP-9633 Python 3.9 + deps updates (#22)

--- setup.py
+++ setup.py
@@ -23,6 +23,12 @@ setup(
     description="Flask blueprint and utilities for oauth2 client",
     url="https://github.com/uc-cdis/cdis_oauth2client",
     license="Apache",
-    packages=["cdis_oauth2client",],
-    install_requires=["Flask", "requests~=2.5", "cdispyutils>=1.0.4",],
+    packages=[
+        "cdis_oauth2client",
+    ],
+    install_requires=[
+        "Flask>=1.1.1",
+        "requests>=2.5",
+        "cdispyutils>=1.0.4",
+    ],
 )
